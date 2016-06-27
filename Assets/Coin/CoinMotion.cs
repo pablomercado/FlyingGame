b61b461e6926f2e916986ad40d8e875bf6522693
Pablo <pablomercadomx@gmail.com>
2016-06-27 20:13:44 +0700

    coin

---
+++ Assets/Coin/CoinMotion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinMotion : MonoBehaviour {
+
+    [SerializeField]
+    public float multiplier;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currentPos = transform.position;
+        transform.position = new Vector3(currentPos.x, Mathf.Sin(Time.time + multiplier), currentPos.z);
+
+        transform.eulerAngles = new Vector3(Time.time * 200f, 0f, 0f);
+	}
+}
---
+++ Assets/CoinSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawner : MonoBehaviour {
+    public GameObject coinGO;
+    // Use this for initialization
+
+    private float multiplier = 0;
+
+	void Start () {
+
+        StartCoroutine(instantiateCoins(0.1f));
+	}
+	
+    IEnumerator instantiateCoins(float wait)
+    {
+        multiplier++;
+        yield return new WaitForSeconds(wait);
+        Vector3 position = new Vector3(Time.time, Mathf.Sin(Time.time));
+        Instantiate(coinGO, position, Quaternion.identity);
+        coinGO.GetComponent<CoinMotion>().multiplier = multiplier;
+        StartCoroutine(instantiateCoins(0.3f));
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
